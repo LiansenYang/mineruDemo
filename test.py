25ec41eccbb56464a49dce1cbe506b6c4304bb59
yangls <yangls1086@gmail.com>
2025-04-18 15:47:28 +0800

    添加前后执行时间

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import os
-
+import time  # 引入time模块
 from magic_pdf.data.data_reader_writer import FileBasedDataWriter, FileBasedDataReader
 from magic_pdf.data.dataset import PymuDocDataset
 from magic_pdf.model.doc_analyze_by_custom_model import doc_analyze
 from magic_pdf.data.dataset import SupportedPdfParseMethod
 
+# 记录开始时间
+start_time = time.time()
 # args
 pdf_file_name = "abc.pdf"  # replace with the real pdf path
 name_without_suff = pdf_file_name.split(".")[0]
@@ -37,3 +39,8 @@ else:
     ds.apply(doc_analyze, ocr=False).pipe_txt_mode(image_writer).dump_md(
     md_writer, f"{name_without_suff}.md", image_dir
 )
+
+# 记录结束时间
+end_time = time.time()
+# 打印总共花费时间
+print(f"总共花费时间: {end_time - start_time:.2f} 秒")
